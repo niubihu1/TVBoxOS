89a51dce146c9fbb2bafaa3c273530d0f02e8aca
niubihu1 <89960586+niubihu1@users.noreply.github.com>
2023-02-15 15:15:15 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://raw.githubusercontent.com/niubihu1/tvbox-/main/1.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
